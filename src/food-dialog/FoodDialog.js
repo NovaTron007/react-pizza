d2d2f3ae3234881b236083b685e1df7f247c704d
DwayneMan <dwayne.man@gmail.com>
2020-08-24 20:31:32 +0800

    Created Order container and Order content.

--- src/food-dialog/FoodDialog.js
+++ src/food-dialog/FoodDialog.js
@@ -1,19 +1,50 @@
 import React from "react";
 import styled from "styled-components";
 import { FoodLabel } from "../menu/FoodGrid";
+import { pizzaRed } from "../styles/colours";
+import { Title } from "../styles/title";
 
 // max-height set 100px of viewport minus 100px to create gap
 // left calc half screen width minus 250px of div's with ie 500px
+// display flex and direction set to col, to allow divs expand inside div using overflow
 const Dialog = styled.div`
   width: 500px;
-  height: 500px;
   background-color: #fff;
   position: fixed;
   top: 75px;
   z-index: 5;
   max-height: calc(100% - 100px);
   left: calc(50% - 250px);
+  display: flex;
+  flex-direction: column;
 `;
+
+export const DialogContent = styled.div`
+  min-height: 100px;
+  overflow: auto;
+`;
+
+// export for reuse
+export const DialogFooter = styled.div`
+  box-shadow: 0px 2px 20px 0px grey;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+`;
+
+// bring in Title component for further styling. Export to resuse button
+export const ConfirmButton = styled(Title)`
+  height: 20px;
+  width: 200px;
+  margin: 10px;
+  color: #fff;
+  background-color: ${pizzaRed};
+  border-radius: 5px;
+  padding: 10px;
+  text-align: center;
+  cursor: pointer;
+`;
+
 const DialogShadow = styled.div`
   position: fixed;
   height: 100%;
@@ -54,6 +85,10 @@ export function FoodDialog({ openFood, setOpenFood }) {
         <DialogBanner img={openFood.img}>
           <DialogBannerName>{openFood.name}</DialogBannerName>
         </DialogBanner>
+        <DialogContent></DialogContent>
+        <DialogFooter>
+          <ConfirmButton>Order</ConfirmButton>
+        </DialogFooter>
       </Dialog>
     </>
   ) : null;
--- src/order/Order.js
+++ src/order/Order.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { DialogFooter, DialogContent, ConfirmButton } from "../food-dialog/FoodDialog"; // import resueable styled components
 
+// order container
 const OrderStyled = styled.div`
-position: fixed;
-right:0px;
-top:48px;
-width: 340px;
-background-color: #fff;
-height: calc(100% - 48px);
-z-index: 10px;
-box-shadow: 0px 0px 5px 5px grey;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  right: 0px;
+  top: 48px;
+  width: 340px;
+  background-color: #fff;
+  height: calc(100% - 48px);
+  z-index: 10;
+  box-shadow: 0px 0px 5px 5px grey;
 `;
 
-export function Order(){
-  return <OrderStyled/>
+// order content area, style dialog content further
+const OrderContent = styled(DialogContent)`
+  height: 100%;
+  padding: 20px;
+`;
+
+export function Order() {
+  return (
+    <OrderStyled>
+      <OrderContent>Your order is empty</OrderContent>
+      <DialogFooter>
+        <ConfirmButton>Checkout</ConfirmButton>
+      </DialogFooter>
+    </OrderStyled>
+  );
 }
